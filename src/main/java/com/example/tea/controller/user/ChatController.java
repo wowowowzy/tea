603cd61d1c8dec8a2be45fcec6d10ee4e59eec15
张洋 <16331338+zhang-yang-wow@user.noreply.gitee.com>
2026-03-12 21:50:31 +0800

    ai接口实现历史记录搜索（根据sessionId分组）-优化prompt提示词单独成类

--- src/main/java/com/example/tea/controller/user/ChatController.java
+++ src/main/java/com/example/tea/controller/user/ChatController.java
@@ -1,9 +1,9 @@
 package com.example.tea.controller.user;
 
 import com.example.tea.entity.pojo.ChatModel.History;
+import com.example.tea.entity.pojo.ChatModel.Prompt;
 import com.example.tea.mapper.ChatMapper;
 import com.example.tea.utils.ThreadLocalUserIdUtil;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.messages.AssistantMessage;
 import org.springframework.ai.chat.messages.Message;
@@ -24,13 +24,15 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/user")
-@Slf4j
 public class ChatController {
     @Autowired
     private ChatClient chatClient;
     @Autowired
     private ChatMapper chatMapper;
 
+    /*
+    * 核心ai接口
+    * */
     @GetMapping( "/ai/generateStream")
 	public Flux<String> generateStream
             (@RequestParam(value = "message", defaultValue = "你好") String message,
@@ -47,15 +49,7 @@ public class ChatController {
         List<Message> MessageList = hisories.stream().map(history -> history.getRole().equals("user") ? new UserMessage(history.getContent())
                 : new AssistantMessage(history.getContent())).collect(Collectors.toList());
         StringBuilder[] strBuilder = {new StringBuilder()};
-        Flux<String> stream = chatClient.prompt("你是一位专业、温和、有耐心的茶叶智能助手「小洋」。\n" +
-                        "你熟悉各类茶叶知识，包括红茶、绿茶、白茶、乌龙茶、普洱茶、黑茶、黄茶等，擅长茶叶介绍、功效、冲泡方法、储存方式、选购建议、搭配饮用、茶文化科普。\n" +
-                        "\n" +
-                        "请遵守以下规则：\n" +
-                        "1. 回答简洁易懂，不使用复杂专业术语，语气亲切自然。\n" +
-                        "2. 只回答与茶叶、茶饮、茶文化相关的问题，不回答无关内容。\n" +
-                        "3. 不编造虚假信息，保持专业、准确、实用。\n" +
-                        "4. 不涉及政治、敏感、违法内容。\n" +
-                        "5. 流式输出，逐字回复，不使用复杂格式。")
+        Flux<String> stream = chatClient.prompt(new Prompt().getPrompt())
                 .user(message).messages(MessageList).stream().content();
         Long finalSessionId = sessionId;
         return stream.doOnNext(s -> strBuilder[0].append(s)).doOnComplete( ()-> chatMapper.add(History.builder()
@@ -66,6 +60,8 @@ public class ChatController {
                         .userId(userId)
                 .build()));
     }
+    /*
+    * 根据用userId查询聊天历史记录*/
     @GetMapping("/ai/getHistories")
     public Map<Long,List<History>> getHistories(@RequestParam(value = "userId") Long userId){
         HashMap<LocalDateTime, List<History>> map = new HashMap<>();
