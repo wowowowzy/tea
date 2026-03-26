981ae487d5c7860f27fb20406e1174011669ebf1
张洋 <16331338+zhang-yang-wow@user.noreply.gitee.com>
2026-03-26 23:18:43 +0800

    祈愿项目无Bug...

--- src/main/java/com/example/tea/controller/user/GoodsController.java
+++ src/main/java/com/example/tea/controller/user/GoodsController.java
@@ -3,6 +3,7 @@ package com.example.tea.controller.user;
 import com.example.tea.entity.dto.Goods.GoodsQueryDTO;
 import com.example.tea.entity.pojo.Result;
 import com.example.tea.service.GoodsService;
+import com.example.tea.service.SeckillService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class GoodsController {
     @Autowired
     private GoodsService goodsService;
+    @Autowired
+    private SeckillService seckillService;
 
     /**
      * 商品全查找(包含分类查找)
@@ -34,4 +37,12 @@ public class GoodsController {
         return Result.success(goodsService.findGoodById(goodsId));
     }
 
+    /**
+     * 获取所有秒杀商品
+     * @return
+     */
+    @GetMapping("getAllSeckillGoods")
+    public Result getAllSeckillGoods() {
+        return Result.success(seckillService.getAllSeckillGoods());
+    }
 }
